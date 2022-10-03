becea8c62217ec53da46f8fc0a9485115e35a463
Benjamin Lui <benjaminlui00@gmail.com>
2022-10-03 16:32:29 -0700

    added admin and member role

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ResolutionSigningSystem.Pages;
 
+[Authorize (Roles = "Admin, Member")]
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
