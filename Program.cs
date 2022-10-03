db20ef00ce1306dd94c0a703c49b6261f68ecec9
mark-p7 <markdeguzman1142@gmail.com>
2022-10-03 16:58:18 -0700

    changed from using sqlite to sqlserver run on docker, and remigrated seeded data

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ using ResolutionSigningSystem.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+string connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(
+options => options.UseSqlServer(connStr)
+);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
